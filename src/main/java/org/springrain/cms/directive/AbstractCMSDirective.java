23d6b3b0b3bfaca5d0b9f2b22fb1a5cdcbdb696c
Administrator <Administrator@MACHENI-RDNA5FG>
2018-10-28 21:53:40 +0800

    测试提交

--- src/main/java/org/springrain/cms/directive/AbstractCMSDirective.java
+++ src/main/java/org/springrain/cms/directive/AbstractCMSDirective.java
@@ -22,7 +22,7 @@ import freemarker.template.TemplateModel;
  */
 public abstract class AbstractCMSDirective implements
 		TemplateDirectiveModel {
-//xuqiangceshi	
+//xuqiangceshi	cihang
 	/**
 	 * 输入参数，内容ID
 	 */
